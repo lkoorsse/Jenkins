40cadf11495082fecb87bd04bf8a68972ae5ec0b
lkoorsse <lesakoorsse@hotmail.com>
2018-03-05 11:04:36 +0000

    #Unit_test_with_Jenkins_run_success

--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -5,7 +5,7 @@ using HelloWorld;
 namespace HelloWorldTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1 
     {
         [TestMethod]
         public void TestMethod1()
